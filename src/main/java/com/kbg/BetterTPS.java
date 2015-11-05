22c2e371e83b499fc6eedcd95970b92e0ee6aa3c
Kenny Goodin <kennethbgoodin@gmail.com>
2015-11-05 17:00:12 -0500

    no message

--- src/main/java/com/kbg/BetterTPS.java
+++ src/main/java/com/kbg/BetterTPS.java
@@ -2,6 +2,7 @@ package com.kbg;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Random;
 
 import net.md_5.bungee.api.ChatColor;
 
@@ -16,25 +17,27 @@ public class BetterTPS extends JavaPlugin implements Listener {
     
     public static volatile Integer TPS;
     public static volatile BetterTPS _PLUGIN_INSTANCE;
-    public static final String[] tpsCONFIG = {"t", "p", "s"};
+    public static final String[] tps_CONFIG = {"t", "p", "s"};
     
     @Override
     public void onEnable() {
         _PLUGIN_INSTANCE = this;
         _PLUGIN_INSTANCE.getServer().getPluginManager().registerEvents((Listener) _PLUGIN_INSTANCE, _PLUGIN_INSTANCE);
-        _PLUGIN_INSTANCE.createConfig();
-        _PLUGIN_INSTANCE.TPS = _PLUGIN_INSTANCE.getConfig().getInt(_PLUGIN_INSTANCE.getConfigTps());
+        _PLUGIN_INSTANCE._CREATE_Config();
+        _PLUGIN_INSTANCE.TPS = _PLUGIN_INSTANCE.getConfig().getInt(_PLUGIN_INSTANCE.GetConfigTPS());
         if (_PLUGIN_INSTANCE.TPS > 420) {
             _PLUGIN_INSTANCE.getLogger().info("Highest possible TPS is 420!");
             _PLUGIN_INSTANCE.TPS = 420;
         }
         new BukkitRunnable() {          
             public void run() {
-                Bukkit.getOnlinePlayers().stream().forEach(player -> {
-                    int tps = _PLUGIN_INSTANCE.getConfig().getInt(_PLUGIN_INSTANCE.getConfigTps());
-                    float SPEED = (float) (0.2 * (_PLUGIN_INSTANCE.TPS> 100 ? 5 : _PLUGIN_INSTANCE.TPS/20));
-                    player.setWalkSpeed(SPEED);
-                    player.setFlySpeed(SPEED);
+                Bukkit.getOnlinePlayers().stream().forEach(_PLAYER -> {
+                    int tps = _PLUGIN_INSTANCE.getConfig().getInt(_PLUGIN_INSTANCE.GetConfigTPS());
+                    float SPEED = (float) (0.2 * (_PLUGIN_INSTANCE.TPS> 100 ? 5 : _PLUGIN_INSTANCE.TPS/20)); //super secret algorithm
+                    _PLAYER.setWalkSpeed(SPEED);
+                    _PLAYER.setFlySpeed(SPEED);
+                    Random Random = new Random();
+                    _PLAYER.spigot().setCollidesWithEntities(Random.nextInt(2) == 0); 
                 });
             }
         }.runTaskTimer(_PLUGIN_INSTANCE, 0L, (25/5));
@@ -42,20 +45,20 @@ public class BetterTPS extends JavaPlugin implements Listener {
     
     
     @EventHandler //what does @ even mean not like i'm emailing
-    public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
-        if (event.getMessage().equalsIgnoreCase("/tps")) {
-            event.getPlayer().sendMessage(String.format("%sTPS from last 1m, 5m, 15m: %s*%s.0, *%s.0, *%s.0", ChatColor.GOLD, ChatColor.GREEN, _PLUGIN_INSTANCE.TPS, _PLUGIN_INSTANCE.TPS, _PLUGIN_INSTANCE.TPS));
-            event.setCancelled(1 == 1);
+    public void onPlayerCommand(PlayerCommandPreprocessEvent _EVENT_) {
+        if (_EVENT_.getMessage().equalsIgnoreCase("/tps")) {
+            _EVENT_.getPlayer().sendMessage(String.format("%sTPS from last 1m, 5m, 15m: %s*%s.0, *%s.0, *%s.0", ChatColor.GOLD, ChatColor.GREEN, _PLUGIN_INSTANCE.TPS, _PLUGIN_INSTANCE.TPS, _PLUGIN_INSTANCE.TPS));
+            _EVENT_.setCancelled(1 == 1);
         }
     }
     
-    public static String getConfigTps() {
-        StringBuilder string = new StringBuilder();
-        Arrays.asList(tpsCONFIG).stream().forEach(str -> string.append(str));
-        return string.toString();
+    public static String GetConfigTPS() {
+        StringBuilder string_Builder = new StringBuilder();
+        Arrays.asList(tps_CONFIG).stream().forEach(strinG -> string_Builder.append(strinG));
+        return string_Builder.toString();
     }
     
-    public int createConfig() {
+    public int _CREATE_Config() {
         try {
             if (!_PLUGIN_INSTANCE.getDataFolder().exists()) {
                 _PLUGIN_INSTANCE.getDataFolder().mkdirs();
